d25cf9c9cd6f1f7257ba4303ec761c8c6b282e95
Pankaj Agham <pankajagham1@gmail.com>
2019-02-28 21:12:49 +0530

    AuthorType object type added. Relation between BookType and AuthorType added

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,29 +1,66 @@
 const graphql = require('graphql');
 const _ = require('lodash');
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLID,
+} = graphql;
 
 var books = [
-    {name: "The wings of Fire", genre:"inspiration", id: 1},
-    {name: "What's wrong in that place", genre:"horro", id:2},
-    {name: "Why I am a coder", genre:"inspiration", id:3}
+    {name: "The wings of Fire", genre:"inspiration", id: '1', authorId: '1'},
+    {name: "What's wrong in that place", genre:"horro", id:'2',authorId: '2'},
+    {name: "Why I am a coder", genre:"inspiration", id:'3', authorId: '3'}
 ]
+
+var authors =[
+    {name: "Nagraj manjule", age: 40, id: '1'},
+    {name: "Sanjay lila Bhansali", age: 47, id: '2'},
+    {name: "Raju hirani", age: 45, id: '3'}
+]
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: ()=>({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
-        genre: { type: GraphQLString }
+        genre: { type: GraphQLString },
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                return _.find(authors, {id: parent.authorId})
+            }
+        }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: ()=>({
+        id: { type:GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
     })
 })
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        //book query
         book: {
             type: BookType,
-            args: {id:{type: GraphQLString}},
+            args: {id:{type: GraphQLID}},
             resolve(parent,args){
-                return _.find(books, {id: 2})
+                return _.find(books, {id: args.id})
+            }
+        },
+        //author query
+        author:{
+            type: AuthorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                return _.find(authors, {id: args.id})
             }
         }
     }
@@ -37,4 +74,3 @@ module.exports = new GraphQLSchema({
 //1. Define object Types
 //2. Defien the relation between those object types
 //3. Define how client interact with data from Object Type
-
